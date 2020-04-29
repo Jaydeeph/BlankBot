0928507795f39f5425bcca48de1159bd3c8558fd
Jaydeep Hasmukhlal <jaydeeph.vaja@gmail.com>
2020-04-29 18:11:10 +0100

    Added reload script. Updated avatar with new API

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,21 +1,21 @@
-const Discord = require('discord.js');
+const { MessageEmbed } = require('discord.js');
 
 exports.run = (client, message, args) => {
-	const avatarEmbed = Discord.RichEmbed;
+	const avatarEmbed = new MessageEmbed();
 
 	if (!message.mentions.users.size) {
-		avatarEmbed.setColor('f65c78');
+		avatarEmbed.setColor('#f65c78');
 		avatarEmbed.setTitle(`${message.author.username}'s Avatar`);
-		avatarEmbed.setDescription(`${message.author.displayAvatarURL}` + '?size=1024');
-		avatarEmbed.setImage(`${message.author.displayAvatarURL}` + '?size=1024');
+		avatarEmbed.setDescription(`[External Link](${message.author.displayAvatarURL({ format: 'png', size: 1024 })})`);
+		avatarEmbed.setImage(`${message.author.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 })}`);
 		avatarEmbed.setTimestamp();
 	}
 
 	message.mentions.users.map(user => {
 		avatarEmbed.setColor('f65c78');
 		avatarEmbed.setTitle(`${user.username}'s Avatar`);
-		avatarEmbed.setDescription(`${user.displayAvatarURL}` + '?size=1024');
-		avatarEmbed.setImage(`${user.displayAvatarURL}` + '?size=1024');
+		avatarEmbed.setDescription(`[External Link](${user.displayAvatarURL({ format: 'png', size: 1024 })})`);
+		avatarEmbed.setImage(`${user.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 })}`);
 		avatarEmbed.setTimestamp();
 	});
 
