b93a7189c2b2020ff0b141a1440729d5b62aad82
Jaydeep Hasmukhlal <jaydeeph.vaja@gmail.com>
2020-04-30 20:12:06 +0100

    Modified avatar command

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,14 +1,26 @@
 const { MessageEmbed } = require('discord.js');
 
-exports.run = (client, message, args) => {
+exports.run = async (client, message, args) => {
 	const avatarEmbed = new MessageEmbed();
 
 	if (!message.mentions.users.size) {
-		avatarEmbed.setColor('#f65c78');
-		avatarEmbed.setTitle(`${message.author.username}'s Avatar`);
-		avatarEmbed.setDescription(`[External Link](${message.author.displayAvatarURL({ format: 'png', size: 1024 })})`);
-		avatarEmbed.setImage(`${message.author.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 })}`);
-		avatarEmbed.setTimestamp();
+		if (Object.keys(args).length == 0) {
+			avatarEmbed.setColor('#f65c78');
+			avatarEmbed.setTitle(`${message.author.username}'s Avatar`);
+			avatarEmbed.setDescription(`[External Link](${message.author.displayAvatarURL({ format: 'png', size: 1024 })})`);
+			avatarEmbed.setImage(`${message.author.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 })}`);
+			avatarEmbed.setTimestamp();
+		}
+		// else {
+		// 	const member = message.guild.members.get(args[0]);
+		// 	if(!member) {return message.reply('Please mention a valid member of this server');}
+
+		// 	avatarEmbed.setColor('#f65c78');
+		// 	avatarEmbed.setTitle(`${member.author.username}'s Avatar`);
+		// 	avatarEmbed.setDescription(`[External Link](${member.author.displayAvatarURL({ format: 'png', size: 1024 })})`);
+		// 	avatarEmbed.setImage(`${member.author.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 })}`);
+		// 	avatarEmbed.setTimestamp();
+		// }
 	}
 
 	message.mentions.users.map(user => {
--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,5 @@
 module.exports = (client) => {
 	// This event will run if the bot starts, and logs in, successfully.
 	console.log(`Blank has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
-	client.user.setActivity(`Serving ${client.guilds.size} servers`);
+	client.user.setActivity('What is life without games?');
 };
