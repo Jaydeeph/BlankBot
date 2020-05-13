cc70c4d40b099f743c0c335d2552d51fea6bdef8
Jaydeep Hasmukhlal <jaydeeph.vaja@gmail.com>
2020-05-14 00:49:10 +0100

    Reformatted user model code.

--- events/message.js
+++ events/message.js
@@ -1,5 +1,5 @@
 const { prefix } = require('../config.json');
-const Users = require('../database/models/user.js');
+const userModel = require('../utilities/usersModelUtility.js');
 
 module.exports = async (client, message) => {
 	if(message.author.bot) return;
@@ -7,40 +7,10 @@ module.exports = async (client, message) => {
 	// console.log(member);
 
 	if (message.guild) {
-		Users.findOne({
-			user_id: message.author.id,
-			guild_id: message.guild.id,
-		}, (error, user) => {
-			if (error) console.log(error);
-
-			if (!user) {
-				const newUser = new Users({
-					user_id: message.author.id,
-					guild_id: message.guild.id,
-					guild_name: message.guild.name,
-					gold: 1,
-					xp: 1,
-				});
-				newUser.save();
-			}
-			else {
-				const gold = Math.floor(Math.random() * 5) + 1;
-				const xp = 3;
-
-				user.gold += gold;
-				user.xp += xp;
-
-				const newLevel = Math.floor(0.1 * Math.sqrt(user.xp));
-				if (user.level < newLevel) {
-					user.level = newLevel;
-					message.reply(`Aye, congrats you've just leveled up to level ${newLevel}!`);
-				}
-
-				user.last_wrote = Date.now();
-
-				user.save();
-			}
-		});
+		userModel.createUserIfDoesNotExist(message);
+		userModel.addGoldAndXP(message);
+		userModel.checkUserLevel(message);
+		userModel.addLastWrote(message);
 	}
 
 	if(message.content.indexOf(prefix) !== 0) return;
---
+++ utilities/usersModelUtility.js
@@ -0,0 +1,68 @@
+const Users = require('../database/models/user.js');
+
+module.exports.createUserIfDoesNotExist = async (message) => {
+	Users.findOne({
+		user_id: message.author.id,
+		guild_id: message.guild.id,
+	}, (error, user) => {
+		if (error) console.log(error);
+
+		if (!user) {
+			const newUser = new Users({
+				user_id: message.author.id,
+				guild_id: message.guild.id,
+				guild_name: message.guild.name,
+			});
+
+			newUser.save();
+		}
+	});
+};
+
+module.exports.addGoldAndXP = async (message) => {
+	Users.findOne({
+		user_id: message.author.id,
+		guild_id: message.guild.id,
+	}, (error, user) => {
+		if (error) console.log(error);
+
+		const gold = Math.floor(Math.random() * 5) + 1;
+		const xp = 50;
+
+		user.gold += gold;
+		user.xp += xp;
+
+		user.save();
+	});
+};
+
+module.exports.checkUserLevel = async (message) => {
+	Users.findOne({
+		user_id: message.author.id,
+		guild_id: message.guild.id,
+	}, (error, user) => {
+		if (error) console.log(error);
+
+		const newLevel = Math.floor(0.1 * Math.sqrt(user.xp));
+
+		if (user.level < newLevel) {
+			user.level = newLevel;
+			message.reply(`Aye, congrats you've just leveled up to level ${newLevel}!`);
+		}
+
+		user.save();
+	});
+};
+
+module.exports.addLastWrote = async (message) => {
+	Users.findOne({
+		user_id: message.author.id,
+		guild_id: message.guild.id,
+	}, (error, user) => {
+		if (error) console.log(error);
+
+		user.last_wrote = Date.now();
+
+		user.save();
+	});
+};
